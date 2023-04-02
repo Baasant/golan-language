baee6ad35e828aa5a1e21a2440561e7460ee9e59
Baasant <basantsayd88@gmail.com>
2023-04-02 04:44:33 +0200

    learn how to add delete put get using gin and gorm

--- gin learning/go-crud/controllers/postsController.go
+++ gin learning/go-crud/controllers/postsController.go
@@ -8,10 +8,17 @@ import (
 )
 
 func PostsCreate(c *gin.Context) {
+	// get data off req body
+
+	var body struct {
+		Body  string
+		Title string
+	}
+	c.Bind(&body)
 	// get data of request body
 	// create a post
-	post := models.Post{Title: "hello", Body: "post body"}
-
+	// post := models.Post{Title: "hello bassant", Body: "post body"}
+	post := models.Post{Title: body.Title, Body: body.Body}
 	result := initializers.DB.Create(&post) // pass pointer of data to Create
 
 	// check if there is error
@@ -27,4 +34,75 @@ func PostsCreate(c *gin.Context) {
 		// "message2": "here we used elepentsql",
 		// "message3": "using pstagesql local and tablePlus",
 	})
+}
+
+func PostsIndex(c *gin.Context) {
+	//get the posts
+	var posts []models.Post
+	// find all the posts and assign it to the variable posts
+	initializers.DB.Find(&posts)
+	//respond with them
+	c.JSON(200, gin.H{
+		"posts": posts,
+		// "message":  "keep going Bassant,i love you ,you are doing great job",
+		// "message2": "here we used elepentsql",
+		// "message3": "using pstagesql local and tablePlus",
+	})
+}
+
+// function to fetch single post
+func PostsShow(c *gin.Context) {
+	//get id from url
+	id := c.Param("id")
+	//get the posts
+	var post models.Post
+	// find all the posts and assign it to the variable posts
+	initializers.DB.First(&post, id)
+	//respond with them
+
+	c.JSON(200, gin.H{
+		"post": post,
+		// "message":  "keep going Bassant,i love you ,you are doing great job",
+		// "message2": "here we used elepentsql",
+		// "message3": "using pstagesql local and tablePlus",
+	})
+}
+
+func PostsUpdates(c *gin.Context) {
+	//get id from url
+	id := c.Param("id")
+	//get the data of request body
+	var body struct {
+		Body  string
+		Title string
+	}
+	c.Bind(&body)
+	// find the post you want to  updated
+	var post models.Post
+	// find all the posts and assign it to the variable posts
+	initializers.DB.First(&post, id)
+	//Update it
+	initializers.DB.Model(&post).Updates(models.Post{Title: body.Title,
+		Body: body.Body,
+	})
+
+	//response with it
+	c.JSON(200, gin.H{
+		"post": post,
+		// "message":  "keep going Bassant,i love you ,you are doing great job",
+		// "message2": "here we used elepentsql",
+		// "message3": "using pstagesql local and tablePlus",
+	})
+}
+
+func PostsDlete(c *gin.Context) {
+	//get id from url
+	id := c.Param("id")
+	// delete the posts
+	initializers.DB.Delete(&models.Post{}, id)
+
+	//response with it
+	// get 200 if its deleted correctly
+	c.Status(200)
+
 }
--- gin learning/go-crud/main.go
+++ gin learning/go-crud/main.go
@@ -16,8 +16,13 @@ func init() {
 
 func main() {
 	r := gin.Default()
+	// connect every things to our router
 	// r.GET("/", controllers.PostsCreate)
 	r.POST("/posts", controllers.PostsCreate)
+	r.GET("/posts", controllers.PostsIndex)
+	r.GET("/posts/:id", controllers.PostsShow)
+	r.PUT("/posts/:id", controllers.PostsUpdates)
+	r.DELETE("/posts/:id", controllers.PostsDlete)
 
-	r.Run() // listen and serve on 0.0.0.0:8080
+	r.Run() // listen and serve on 0.0.0.0:8080 the we change it to run on 3000
 }
