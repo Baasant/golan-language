9c58ee12605213368ef7bb1aec090e93d91f2a46
Baasant <basantsayd88@gmail.com>
2023-04-25 05:40:06 +0200

    create CRUD API for movies

--- gin learning/crud_movies_project/main.go
+++ gin learning/crud_movies_project/main.go
@@ -17,7 +17,7 @@ import (
 type Movie struct {
 	ID       string    `json:"id"`
 	Isbn     string    `json:"isbn"`
-	Title    string    `json :"title"`
+	Title    string    `json:"title"`
 	Director *Director `json:"director"`
 }
 
@@ -112,6 +112,6 @@ func main() {
 	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
 
 	fmt.Printf("sRATING SERVER AT PORT 8000")
-	log.Fatal(http.ListenAndServe("8000", r)) //log out if the server doesn't start
+	log.Fatal(http.ListenAndServe(":8000", r)) //log out if the server doesn't start
 
 } //main
